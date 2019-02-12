2a21168ff890028e98e6bd10c7112903073fe6a3
Daniel Čech <daniel.cech@gmail.com>
2019-02-12 17:30:31 +0100

    [feat]: great improvements

--- Sources/ScriptToolkit/ScriptToolkit.swift
+++ Sources/ScriptToolkit/ScriptToolkit.swift
@@ -110,43 +110,93 @@ public func flattenFolderStructure(inputDir: String, outputDir: String, move: Bo
     }
 }
 
-public func exifTool(inputDir: String) throws {
+public func exifTool(inputDir: String, byCameraModel: Bool, processM4V: Bool) throws {
     let inputFolder = try Folder(path: inputDir)
 
     print("📷 Processing EXIFtool...")
 
-    // Process dirs using exiftool
+    if byCameraModel {
+        // Process JPG dirs using exiftool
+        for dir in inputFolder.subfolders {
+            run("/usr/local/bin/exiftool","-Directory<Model", "-d", "%Y-%m-%d \(dir.name)", dir.path)
+        }
+
+        // Process JPG using exiftool
+        for file in inputFolder.files {
+            run("/usr/local/bin/exiftool" ,"-Directory<Model", "-d", "%Y-%m-%d", file.path)
+        }
+        return
+    }
+
+
+    // Process JPG dirs using exiftool
     for dir in inputFolder.subfolders {
         run("/usr/local/bin/exiftool","-Directory<DateTimeOriginal", "-d", "%Y-%m-%d \(dir.name)", dir.path)
     }
 
-    // Process files using exiftool
-    for file in inputFolder.files {
-        run("/usr/local/bin/exiftool" ,"-Directory<DateTimeOriginal", "-d", "%Y-%m-%d", file.path)
+    var originalFolders = inputFolder
+        .subfolders
+        .filter { matches(for: "^\\d\\d\\d\\d-\\d\\d-\\d\\d.*$", in: $0.name).isEmpty }
+
+    originalFolders.append(inputFolder)
+
+    // Support of M4V
+    for dir in originalFolders {
+        for file in dir.makeFileSequence(recursive: true, includeHidden: true) {
+
+            switch file.extension?.lowercased() ?? "" {
+            case "jpg", "jpeg":
+                run("/usr/local/bin/exiftool" ,"-Directory<DateTimeOriginal", "-d", "%Y-%m-%d", file.path)
+
+            case "png":
+                run("/usr/local/bin/exiftool" ,"-Directory<DateCreated", "-d", "%Y-%m-%d", file.path)
+
+            case "m4v":
+                if !processM4V {
+                    run("/usr/local/bin/exiftool" ,"-Directory<ContentCreateDate", "-d", "%Y-%m-%d", file.path)
+                }
+
+            case "mp4":
+                run("/usr/local/bin/exiftool" ,"-Directory<FileAccessDate", "-d", "%Y-%m-%d", file.path)
+
+            case "mov":
+                run("/usr/local/bin/exiftool" ,"-Directory<CreationDate", "-d", "%Y-%m-%d", file.path)
+
+            default:
+                break
+            }
+        }
     }
 }
 
 func incorporateFile(_ file: File, using folderRecords: [(Folder, [Int])]) throws {
+    print("\(file.path)")
     let numberString = file.nameExcludingExtension.replacingOccurrences(of: "IMG_", with: "")
     var lastMaximum: Int?
     if let number = Int(numberString) {
 
+        var moved = false
         for folderRecord in folderRecords {
             if let firstIndex = folderRecord.1.first, let lastIndex = folderRecord.1.last, number >= firstIndex, number <= lastIndex {
                 try file.move(to: folderRecord.0)
+                //                print("  moved to \(folderRecord.0.path)")
+                moved = true
                 break
             }
 
             if let unwrappedLastMaximum = lastMaximum, let firstIndex = folderRecord.1.first, unwrappedLastMaximum <= number, firstIndex >= number {
                 try file.move(to: folderRecord.0)
+                //                print("  moved to \(folderRecord.0.path)")
+                moved = true
                 break
             }
 
             lastMaximum = folderRecord.1.last
         }
+        if !moved { print("  unable to process - no appropriate folder") }
     }
     else {
-        main.stderror.print("\(file.name): unable to process")
+        print("  unable to process")
     }
 }
 
@@ -169,16 +219,14 @@ public func organizePhotos(inputDir: String) throws {
         folderRecords.append((dir, indexes))
     }
 
-    for file in inputFolder.files {
-        try incorporateFile(file, using: folderRecords)
-    }
-
-    let originalFolders = inputFolder
+    var originalFolders = inputFolder
         .subfolders
         .filter { matches(for: "^\\d\\d\\d\\d-\\d\\d-\\d\\d.*$", in: $0.name).isEmpty }
 
-    for dir in originalFolders {
-        for file in dir.makeFileSequence(recursive: true, includeHidden: true) {
+    originalFolders.append(inputFolder)
+
+    for folder in originalFolders {
+        for file in folder.makeFileSequence(recursive: true, includeHidden: true) {
             try incorporateFile(file, using: folderRecords)
         }
     }
