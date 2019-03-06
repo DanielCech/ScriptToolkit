e8a7b503762df0ee34fc4fda0df65971160997fa
Daniel Čech <daniel.cech@gmail.com>
2019-03-06 16:37:06 +0100

    [feat]: printable errors

--- Sources/ScriptToolkit/ScriptToolkit.swift
+++ Sources/ScriptToolkit/ScriptToolkit.swift
@@ -8,7 +8,60 @@
 import Foundation
 import Files
 import SwiftShell
+import Moderator
 
+public protocol PrintableError {
+    var errorDescription: String { get }
+}
+
+public enum ScriptError: Error {
+    case generalError(message: String)
+    case fileExists(message: String)
+    case fileNotFound(message: String)
+    case folderExists(message: String)
+    case folderNotFound(message: String)
+    case argumentError(message: String)
+    case moreInfoNeeded(message: String)
+}
+
+extension ScriptError: PrintableError {
+    public var errorDescription: String {
+        let prefix = "💥 error: "
+        var errorDescription = ""
+
+        switch self {
+        case let .generalError(message):
+            errorDescription = message
+
+        case let .fileExists(message):
+            errorDescription = "file exists: \(message)"
+
+        case let .fileNotFound(message):
+            errorDescription = "file not found: \(message)"
+
+        case let .folderExists(message):
+            errorDescription = "folder exists: \(message)"
+
+        case let .folderNotFound(message):
+            errorDescription = "folder not found: \(message)"
+
+        case let .argumentError(message):
+            errorDescription = "invalid argument: \(message)"
+
+        case let .moreInfoNeeded(message):
+            errorDescription = "more info needed: \(message)"
+        }
+
+        return prefix + errorDescription
+    }
+}
+
+
+extension ArgumentError: PrintableError {
+    public var errorDescription: String {
+        return "💥 error: \(errormessage)"
+    }
+}
 
 public struct ScriptToolkit {
 
