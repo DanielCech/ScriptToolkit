5b368d3c4f17d66ebd79cd87dabaeefb33f6ef6a
Daniel Cech <daniel.cech@strv.com>
2020-07-23 12:18:28 +0200

    [feat]: comparing images

--- Sources/ScriptToolkit/NSImageExtension.swift
+++ Sources/ScriptToolkit/NSImageExtension.swift
@@ -9,17 +9,17 @@ import Foundation
 import AppKit
 
 public extension NSImage {
-
+    
     /// Returns the height of the current image.
     var height: CGFloat {
         return self.size.height
     }
-
+    
     /// Returns the width of the current image.
     var width: CGFloat {
         return self.size.width
     }
-
+    
     var CGImage: CGImage {
         get {
             let imageData = self.tiffRepresentation!
@@ -30,14 +30,14 @@ public extension NSImage {
     }
     
     /// Returns a png representation of the current image.
-    var PNGRepresentation: Data? {
+    var pngRepresentation: Data? {
         if let tiff = self.tiffRepresentation, let tiffData = NSBitmapImageRep(data: tiff) {
             return tiffData.representation(using: .png, properties: [:])
         }
-
+        
         return nil
     }
-
+    
     ///  Copies the current image and resizes it to the given size.
     ///
     ///  - parameter size: The size of the new image.
@@ -46,76 +46,80 @@ public extension NSImage {
     func copy(size: NSSize) throws -> NSImage {
         // Create a new rect with given width and height
         let frame = NSMakeRect(0, 0, size.width, size.height)
-
+        
         // Get the best representation for the given size.
         guard let rep = self.bestRepresentation(for: frame, context: nil, hints: nil) else {
             throw ScriptError.generalError(message: "Unable to resize image")
         }
-
+        
         // Create an empty image with the given size.
         let img = NSImage(size: size)
-
+        
         // Set the drawing context and make sure to remove the focus before returning.
         img.lockFocus()
         defer { img.unlockFocus() }
-
+        
         // Draw the new image
         if rep.draw(in: frame) {
             return img
         }
-
+        
         // Return nil in case something went wrong.
         throw ScriptError.generalError(message: "Unable to resize image")
     }
     
-//    func areImagesSame(firstPNGData: Data, secondPNGData: Data) -> Bool {
-//        let sequence = Data([0x6C, 0x65, 0x58, 0x49, 0x66])
+        func areImagesSame(leftImage: NSImage, rightImage: NSImage) -> Bool {
+            guard let leftPNGData = leftImage.pngRepresentation, let rightPNGData = rightImage.pngRepresentation else { return false }
+            
+            let sequence = Data([0x6C, 0x65, 0x58, 0x49, 0x66])
+    
+            guard let leftOffset = leftPNGData.indexOf(data: sequence) else { return false }
+            let leftSubdata = leftPNGData.subdata(in: leftOffset ..< leftPNGData.endIndex)
+    
+            guard let rightOffset = rightPNGData.indexOf(data: sequence) else { return false }
+            let rightSubdata = rightPNGData.subdata(in: rightOffset ..< rightPNGData.endIndex)
+    
+            return leftSubdata == rightSubdata
+        }
+    
+//    func areImagesSame(leftImage: NSImage, rightImage: NSImage) -> Bool {
+//        let width = Int(leftImage.size.width)
+//        let height = Int(leftImage.size.height)
 //
-//        guard let firstOffset = firstPNGData.indexOf(data: sequence) else { return false }
-//        let firstSubdata = firstPNGData.subdata(in: firstOffset ..< firstPNGData.endIndex)
+//        guard leftImage.size == rightImage.size else {
+//            return false
+//        }
+//        var changes = 0
 //
-//        guard let secondOffset = secondPNGData.indexOf(data: sequence) else { return false }
-//        let secondSubdata = secondPNGData.subdata(in: secondOffset ..< secondPNGData.endIndex)
+//        if let cfData1:CFData = leftImage.CGImage.dataProvider?.data,
+//            let l = CFDataGetBytePtr(cfData1),
+//            let cfData2:CFData = rightImage.CGImage.dataProvider?.data,
+//            let r = CFDataGetBytePtr(cfData2) {
+//            let bytesPerpixel = 4
+//            let firstPixel = 0
+//            let lastPixel = (width * height - 1) * bytesPerpixel
+//            let range = stride(from: firstPixel, through: lastPixel, by: bytesPerpixel)
+//            for pixelAddress in range {
+//                if l.advanced(by: pixelAddress).pointee != r.advanced(by: pixelAddress).pointee ||     //Red
+//                    l.advanced(by: pixelAddress + 1).pointee != r.advanced(by: pixelAddress + 1).pointee || //Green
+//                    l.advanced(by: pixelAddress + 2).pointee != r.advanced(by: pixelAddress + 2).pointee || //Blue
+//                    l.advanced(by: pixelAddress + 3).pointee != r.advanced(by: pixelAddress + 3).pointee  {  //Alpha
+//                    changes += 1
+//                }
+//            }
+//        }
 //
-//        return firstSubdata == secondSubdata
+//        return changes < 100
 //    }
     
-    func areImagesSame(leftImage: NSImage, rightImage: NSImage) -> Bool {
-         let width = Int(leftImage.size.width)
-         let height = Int(leftImage.size.height)
-         guard leftImage.size == rightImage.size else {
-             return false
-         }
-        if let cfData1:CFData = leftImage.CGImage.dataProvider?.data,
-            let l = CFDataGetBytePtr(cfData1),
-            let cfData2:CFData = rightImage.CGImage.dataProvider?.data,
-            let r = CFDataGetBytePtr(cfData2) {
-             let bytesPerpixel = 4
-             let firstPixel = 0
-             let lastPixel = (width * height - 1) * bytesPerpixel
-             let range = stride(from: firstPixel, through: lastPixel, by: bytesPerpixel)
-             for pixelAddress in range {
-                 if l.advanced(by: pixelAddress).pointee != r.advanced(by: pixelAddress).pointee ||     //Red
-                    l.advanced(by: pixelAddress + 1).pointee != r.advanced(by: pixelAddress + 1).pointee || //Green
-                    l.advanced(by: pixelAddress + 2).pointee != r.advanced(by: pixelAddress + 2).pointee || //Blue
-                    l.advanced(by: pixelAddress + 3).pointee != r.advanced(by: pixelAddress + 3).pointee  {  //Alpha
-                     print(pixelAddress)
-                     
-                    return false
-                 }
-             }
-         }
-        
-        return true
-     }
-
     ///  Saves the PNG representation of the current image to the HD.
     ///
     /// - parameter url: The location url to which to write the png file.
     func savePNGRepresentationToURL(url: URL, onlyChange: Bool = true) throws {
-        if let pngData = self.PNGRepresentation {
+        if let pngData = self.pngRepresentation {
             if let originalImage = NSImage(contentsOf: url) {
                 if onlyChange && areImagesSame(leftImage: self, rightImage: originalImage) {
+                    print("    Keeping original file - no change")
                     return
                 }
             }
@@ -135,22 +139,22 @@ public extension NSImage {
             else {
                 throw ScriptError.generalError(message: "Image processing error")
         }
-
+        
         let filter = CIFilter(name: "CISourceOverCompositing")!
         filter.setDefaults()
-
+        
         filter.setValue(firstImage, forKey: "inputImage")
         filter.setValue(secondImage, forKey: "inputBackgroundImage")
-
+        
         let resultImage = filter.outputImage
-
+        
         let rep = NSCIImageRep(ciImage: resultImage!)
         let finalResult = NSImage(size: rep.size)
         finalResult.addRepresentation(rep)
-
+        
         return finalResult
     }
-
+    
     /// Image annotation
     func image(
         withText text: String,
@@ -162,7 +166,7 @@ public extension NSImage {
         let image = self
         let text = text as NSString
         let options: NSString.DrawingOptions = [.usesLineFragmentOrigin, .usesFontLeading]
-
+        
         let textSize = text.boundingRect(with: image.size, options: options, attributes: attributes).size
         var offsetX: CGFloat
         
@@ -176,16 +180,16 @@ public extension NSImage {
         default:
             offsetX = 0
         }
-
+        
         let point = NSMakePoint(image.size.width * horizontalTitlePosition + offsetX, image.size.height * verticalTitlePosition - textSize.height/2)
-
+        
         image.lockFocus()
         text.draw(at: point, withAttributes: attributes)
         image.unlockFocus()
-
+        
         return image
     }
-
+    
     /// Image annotation
     func annotate(
         text: String,
@@ -198,9 +202,9 @@ public extension NSImage {
         stroke: NSColor,
         strokeWidth: CGFloat
     ) throws -> NSImage {
-
+        
         guard let titleFont = NSFont(name: font, size: size) else { throw ScriptError.argumentError(message: "Unable to find font \(font)") }
-
+        
         var alignmentMode: NSTextAlignment
         
         switch titleAlignment {
@@ -228,8 +232,8 @@ public extension NSImage {
             ],
             horizontalTitlePosition: horizontalTitlePosition,
             verticalTitlePosition: verticalTitlePosition
-            )
-
+        )
+        
         // Add strokes
         return fillText.image(
             withText: text,
